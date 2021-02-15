9e872a03dc377b7d2ec147b3f9ba2275b4351ff2
iDingDong <iDingDong@outlook.com>
2021-02-15 12:10:59 +0800

    Feature: Add 'dcool::core::stepOver'
    The type stepped over may be specified by either type of pointer or
    explicit template argument.

--- include/dcool/core/raw_pointer_operation.hpp
+++ include/dcool/core/raw_pointer_operation.hpp
@@ -12,6 +12,16 @@ namespace dcool::core {
 		return static_cast<::dcool::core::IdenticallyQualifiedType<::dcool::core::Byte, T_>*>(generalPointer_) + step_;
 	}
 
+	template <typename T_> constexpr auto stepOver(T_* pointer_) noexcept -> ::dcool::core::IdenticallyQualifiedType<void, T_>* {
+		return ::dcool::core::stepByByte(pointer_, sizeof(*pointer_));
+	}
+
+	template <typename T_> constexpr auto stepOver(
+		::dcool::core::IdenticallyQualifiedType<void, T_>* pointer_
+	) noexcept -> ::dcool::core::IdenticallyQualifiedType<void, T_>* {
+		return ::dcool::core::stepByByte(pointer_, sizeof(*pointer_));
+	}
+
 	inline constexpr auto differenceByByte(
 		void const volatile* left_, void const volatile* right_
 	) noexcept -> ::dcool::core::Difference {
