e0c66ca5672d565664907410bd3d236aabebf037
iDingDong <iDingDong@outlook.com>
2021-02-28 11:43:09 +0800

    Test: Improve linked tests
    Added more checks to existing test cases.

--- test/container/linked.cpp
+++ test/container/linked.cpp
@@ -3,17 +3,53 @@
 #include <dcool/test.hpp>
 
 DCOOL_TEST_CASE(dcoolContainer, linkedBasics) {
-	dcool::container::ForwardLinked<int> linked1;
-	linked1.emplaceFront(1);
-	DCOOL_TEST_EXPECT_RANGE_EQUALITY(linked1, DCOOL_TEST_SEQUENCE(1));
-	linked1.emplaceFront(2);
-	DCOOL_TEST_EXPECT_RANGE_EQUALITY(linked1, DCOOL_TEST_SEQUENCE(2, 1));
-	linked1.emplaceFront(3);
-	DCOOL_TEST_EXPECT_RANGE_EQUALITY(linked1, DCOOL_TEST_SEQUENCE(3, 2, 1));
-	dcool::container::ForwardLinked<int> linked2 = linked1;
-	linked1.eraseAfter(linked1.begin());
-	DCOOL_TEST_EXPECT_RANGE_EQUALITY(linked1, DCOOL_TEST_SEQUENCE(3, 1));
-	DCOOL_TEST_EXPECT_RANGE_EQUALITY(linked2, DCOOL_TEST_SEQUENCE(3, 2, 1));
+	struct Traced {
+		static auto livingCount() -> dcool::core::Length& {
+			static dcool::core::Length count = 0;
+			return count;
+		}
+
+		int value;
+
+		Traced(int inputValue): value(inputValue) {
+			++(livingCount());
+		}
+
+		Traced(Traced const& other): value(other.value) {
+			++(livingCount());
+		}
+
+		~Traced() {
+			--(livingCount());
+		}
+
+		auto operator ==(Traced const&) const -> dcool::core::Boolean = default;
+	};
+
+	using Linked = dcool::container::ForwardLinked<Traced>;
+	{
+		Linked linked1;
+		DCOOL_TEST_EXPECT(Traced::livingCount() == 0);
+		linked1.emplaceFront(1);
+		DCOOL_TEST_EXPECT(Traced::livingCount() == 1);
+		DCOOL_TEST_EXPECT_RANGE_EQUALITY(linked1, DCOOL_TEST_SEQUENCE_OF(Traced, 1));
+		linked1.emplaceFront(2);
+		DCOOL_TEST_EXPECT(Traced::livingCount() == 2);
+		DCOOL_TEST_EXPECT_RANGE_EQUALITY(linked1, DCOOL_TEST_SEQUENCE_OF(Traced, 2, 1));
+		linked1.emplaceFront(3);
+		DCOOL_TEST_EXPECT(Traced::livingCount() == 3);
+		DCOOL_TEST_EXPECT_RANGE_EQUALITY(linked1, DCOOL_TEST_SEQUENCE_OF(Traced, 3, 2, 1));
+		{
+			Linked linked2 = linked1;
+			DCOOL_TEST_EXPECT(Traced::livingCount() == 6);
+			linked1.eraseAfter(linked1.begin());
+			DCOOL_TEST_EXPECT(Traced::livingCount() == 5);
+			DCOOL_TEST_EXPECT_RANGE_EQUALITY(linked1, DCOOL_TEST_SEQUENCE_OF(Traced, 3, 1));
+			DCOOL_TEST_EXPECT_RANGE_EQUALITY(linked2, DCOOL_TEST_SEQUENCE_OF(Traced, 3, 2, 1));
+		}
+		DCOOL_TEST_EXPECT(Traced::livingCount() == 2);
+	}
+	DCOOL_TEST_EXPECT(Traced::livingCount() == 0);
 }
 
 DCOOL_TEST_CASE(dcoolContainer, linkedWithConcretePool) {
@@ -21,16 +57,51 @@ DCOOL_TEST_CASE(dcoolContainer, linkedWithConcretePool) {
 		using Pool = dcool::resource::ConcreteReferencePool<8, dcool::core::storageRequirement<8, 4>>;
 	};
 
-	dcool::container::ForwardLinked<int, Config> linked1;
-	linked1.emplaceFront(1);
-	DCOOL_TEST_EXPECT_RANGE_EQUALITY(linked1, DCOOL_TEST_SEQUENCE(1));
-	linked1.emplaceFront(2);
-	DCOOL_TEST_EXPECT_RANGE_EQUALITY(linked1, DCOOL_TEST_SEQUENCE(2, 1));
-	linked1.emplaceFront(3);
-	DCOOL_TEST_EXPECT_RANGE_EQUALITY(linked1, DCOOL_TEST_SEQUENCE(3, 2, 1));
-	DCOOL_TEST_EXPECT_RANGE_EQUALITY(linked1, DCOOL_TEST_SEQUENCE(3, 2, 1));
-	dcool::container::ForwardLinked<int, Config> linked2 = linked1;
-	linked1.eraseAfter(linked1.begin());
-	DCOOL_TEST_EXPECT_RANGE_EQUALITY(linked1, DCOOL_TEST_SEQUENCE(3, 1));
-	DCOOL_TEST_EXPECT_RANGE_EQUALITY(linked2, DCOOL_TEST_SEQUENCE(3, 2, 1));
+	struct Traced {
+		static auto livingCount() -> dcool::core::Length& {
+			static dcool::core::Length count = 0;
+			return count;
+		}
+
+		int value;
+
+		Traced(int inputValue): value(inputValue) {
+			++(livingCount());
+		}
+
+		Traced(Traced const& other): value(other.value) {
+			++(livingCount());
+		}
+
+		~Traced() {
+			--(livingCount());
+		}
+
+		auto operator ==(Traced const&) const -> dcool::core::Boolean = default;
+	};
+
+	using Linked = dcool::container::ForwardLinked<Traced, Config>;
+	{
+		Linked linked1;
+		DCOOL_TEST_EXPECT(Traced::livingCount() == 0);
+		linked1.emplaceFront(1);
+		DCOOL_TEST_EXPECT(Traced::livingCount() == 1);
+		DCOOL_TEST_EXPECT_RANGE_EQUALITY(linked1, DCOOL_TEST_SEQUENCE_OF(Traced, 1));
+		linked1.emplaceFront(2);
+		DCOOL_TEST_EXPECT(Traced::livingCount() == 2);
+		DCOOL_TEST_EXPECT_RANGE_EQUALITY(linked1, DCOOL_TEST_SEQUENCE_OF(Traced, 2, 1));
+		linked1.emplaceFront(3);
+		DCOOL_TEST_EXPECT(Traced::livingCount() == 3);
+		DCOOL_TEST_EXPECT_RANGE_EQUALITY(linked1, DCOOL_TEST_SEQUENCE_OF(Traced, 3, 2, 1));
+		{
+			Linked linked2 = linked1;
+			DCOOL_TEST_EXPECT(Traced::livingCount() == 6);
+			linked1.eraseAfter(linked1.begin());
+			DCOOL_TEST_EXPECT(Traced::livingCount() == 5);
+			DCOOL_TEST_EXPECT_RANGE_EQUALITY(linked1, DCOOL_TEST_SEQUENCE_OF(Traced, 3, 1));
+			DCOOL_TEST_EXPECT_RANGE_EQUALITY(linked2, DCOOL_TEST_SEQUENCE_OF(Traced, 3, 2, 1));
+		}
+		DCOOL_TEST_EXPECT(Traced::livingCount() == 2);
+	}
+	DCOOL_TEST_EXPECT(Traced::livingCount() == 0);
 }
