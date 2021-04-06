e386bf3cdfb4f170c39bb07c667db9974b6219f1
iDingDong <iDingDong@outlook.com>
2021-04-06 19:59:33 +0800

    Feature: Add 'dcool::version'
    Add constant 'dcool::version'. Expected to extend in the future to
    support ordering.

--- include/dcool/basic.hpp
+++ include/dcool/basic.hpp
@@ -6,11 +6,21 @@
 #	include <cstdint>
 
 namespace dcool {
-	constexpr std::uintmax_t majorVersion = DCOOL_MAJOR_VERSION;
-	constexpr std::uintmax_t minorVersion = DCOOL_MINOR_VERSION;
-	constexpr std::uintmax_t patchVersion = DCOOL_PATCH_VERSION;
+	constexpr ::std::uintmax_t majorVersion = DCOOL_MAJOR_VERSION;
+	constexpr ::std::uintmax_t minorVersion = DCOOL_MINOR_VERSION;
+	constexpr ::std::uintmax_t patchVersion = DCOOL_PATCH_VERSION;
 	constexpr char8_t versionName[] = DCOOL_VERSION_NAME;
 	constexpr char versionNativeName[] = DCOOL_VERSION_NATIVE_NAME;
+
+	struct Version {
+		::std::uintmax_t major = ::dcool::majorVersion;
+		::std::uintmax_t minor = ::dcool::minorVersion;
+		::std::uintmax_t patch = ::dcool::patchVersion;
+		char8_t const* name = ::dcool::versionName;
+		char const* nativeName = ::dcool::versionNativeName;
+	};
+
+	constexpr ::dcool::Version version;
 }
 
 #endif
